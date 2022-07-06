451251da28bf20c59e5928cf65f2c1de451a9eea
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-07-06 19:02:15 +0200

    Divide code into components

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+            <small>© 2022 Brozek development. All rights reserved.
+            </small>
+        </div >
+    )
+}
---
+++ src/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const MainContent = () => {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a famous library.</li>
+                <li>I'm more likely to get a job with this library.</li>
+            </ol>
+        </div>
+    )
+}
